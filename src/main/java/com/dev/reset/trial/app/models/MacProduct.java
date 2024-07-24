aa4b10cc5d161fae1b7f24e35bc58880156cf92f
XGilmarDev <gilmar01eduardo@gmail.com>
2024-07-24 18:03:46 -0500

    update MacProduct

--- src/main/java/com/dev/reset/trial/app/models/MacProduct.java
+++ src/main/java/com/dev/reset/trial/app/models/MacProduct.java
@@ -3,54 +3,109 @@ package com.dev.reset.trial.app.models;
 import com.dev.reset.trial.app.view.AppView;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MacProduct {
     private static final String SCRIPT_FILENAME = "mac.sh";
 
-    public void resetProductMac() {
+    public void resetProductMac(List<String> names) {
         try {
-            createAndRunScriptMac();
+            createAndRunScriptMac(names);
         } catch (IOException | InterruptedException ex) {
             Logger.getLogger(MacProduct.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
 
-    private void createAndRunScriptMac() throws IOException, InterruptedException {
-        String bashCommands = generateSingleBashCommandMac();
+    private void createAndRunScriptMac(List<String> product) throws IOException, InterruptedException {
+        List<String> bashCommands = generateBashCommandsMac(product);
         File scriptFile = createScriptFileMac(bashCommands);
         scriptFile.setExecutable(true);
         executeScriptMac();
         deleteScriptFileMac(scriptFile);
     }
 
-    private String generateSingleBashCommandMac() {
-
-        return """
-                #!/bin/bash
-                prefs_file=~/Library/Preferences/com.apple.java.util.prefs.plist
-                echo "[ INFO ] Removing prefs..."
-                if rm "$prefs_file"; then
-                    echo "[ OK ] Preferences removed successfully."
-                else
-                    echo "[ ERROR ] Failed to remove preferences."
-                fi
-               """;
+    private List<String> generateBashCommandsMac(List<String> products) {
+        List<String> bashCommands = new ArrayList<>();
+        for (String product : products) {
+            bashCommands.add(generateSingleBashCommandMac(product));
+        }
+        return bashCommands;
     }
 
-    private File createScriptFileMac(String bashCommands) throws IOException {
+    private String generateSingleBashCommandMac(String product) {
+        return """
+                 #!/bin/bash
+                 product_name=%s
+                                
+                 prefs_file_one=~/Library/Preferences/com.apple.java.util.prefs.plist
+                 prefs_file_two=~/Library/Preferences/com.jetbrains.*.plist
+                 prefs_file_three=~/Library/Preferences/jetbrains.*.*.plist
+                                
+                 prefs_files=($prefs_file_one $prefs_file_two $prefs_file_three)
+                                
+                 echo "[ INFO ] Resetting period for $product_name"
+                                
+                    echo "Removing evaluation key..."
+                    if rm -rf ~/Library/Preferences/$product_name*/eval; then
+                       echo "[ OK ] Key removed successfully."\s
+                    else
+                       echo "[ ERROR ] Failed to remove key."
+                    fi
+                    
+                    # Above path not working on latest version. Fixed below
+                    if rm -rf ~/Library/Application\\ Support/JetBrains/$product_name/eval; then
+                       echo "[ OK ] Latest version removed successfully."
+                    else
+                       echo "[ ERROR ] Failed to remove latest version."
+                    fi
+                    
+                    echo "Removing evlsprt in options.xml..."
+                    sed -i '' '/evlsprt/d' ~/Library/Preferences/$product_name*/options/other.xml
+                    
+                    if [ $? -eq 0 ]; then
+                        echo "[ OK ] evlsprt removed successfully."
+                    else
+                        echo "[ ERROR ] Failed to remove evlsprt."
+                    fi
+                    
+                    # Above path not working on latest version. Fixed below
+                    sed -i '' '/evlsprt/d' ~/Library/Application\\ Support/JetBrains/$product_name*/options/other.xml
+                    if [ $? -eq 0 ]; then
+                        echo "[ OK ] Latest evlsprt removed successfully."
+                    else
+                        echo "[ ERROR ] Failed to remove latest evlsprt."
+                    fi
+                              
+                                
+                  echo "[ INFO ] removing additional plist files..."
+                                
+                    for file in ${prefs_files[@]}; do
+                        echo "[ INFO ] Removing ${file##*/}..."
+                          if rm "$file"; then
+                             echo "[ OK ] ${file##*/} removed successfully."
+                          else
+                             echo "[ ERROR ] Failed to remove ${file##*/}."
+                          fi
+                    done
+                                
+                """.formatted(product);
+    }
+
+    private File createScriptFileMac(List<String> bashCommands) throws IOException {
         File scriptFile = new File(SCRIPT_FILENAME);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(scriptFile))) {
-            if (bashCommands != null) {
-                writer.write(bashCommands);
+            for (String command : bashCommands) {
+                writer.write(command);
+                writer.newLine();
             }
 
         }
         return scriptFile;
     }
-
 
 
     private void executeScriptMac() throws IOException, InterruptedException {
