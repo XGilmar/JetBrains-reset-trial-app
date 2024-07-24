e1cedb00594bd672d8670aac854c17fa05f37edb
XGilmarDev <gilmar01eduardo@gmail.com>
2024-07-24 16:07:35 -0500

    update ItemComponent

--- src/main/java/com/dev/reset/trial/app/view/components/items/ItemComponent.java
+++ src/main/java/com/dev/reset/trial/app/view/components/items/ItemComponent.java
@@ -1,11 +1,10 @@
 package com.dev.reset.trial.app.view.components.items;
 
 import com.formdev.flatlaf.FlatClientProperties;
-import java.awt.Dimension;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class ItemComponent extends JPanel {
 
@@ -22,21 +21,25 @@ public class ItemComponent extends JPanel {
         labelText = new JLabel();
         checkBox = new JCheckBox();
 
-        MigLayout layout = new MigLayout("", "[][]push[]");
+        MigLayout layout = new MigLayout(null, "[][]push[]");
+        setLayout(layout);
+
         putClientProperty(FlatClientProperties.STYLE, "arc:10;"
                 + "background:lighten(@background,3%);");
+
         setMinimumSize(new Dimension(180, 38));
         setMaximumSize(new Dimension(180, 38));
         setPreferredSize(new Dimension(180, 38));
-        setLayout(layout);
 
         labelText.putClientProperty(FlatClientProperties.STYLE, "font:bold +1");
         checkBox.setFocusable(false);
 
-        add(labelIcon);
-        add(labelText);
-        add(checkBox);
+        add(labelIcon, "cell 0 0");
+        add(labelText, "cell 1 0");
+        add(checkBox, "cell 2 0");
 
+        revalidate();
+        repaint();
     }
 
 }
